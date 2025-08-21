102a5d4542093a3ae134c5103b477970eefd87c0
pingmypoo <132787185+pingmypoo@users.noreply.github.com>
2025-08-21 14:17:46 +0700

    Update ex3.sql

--- ex3.sql
+++ ex3.sql
@@ -9,10 +9,10 @@ From Products as p, Categories as c
 Where p.CategoryID = c.CategoryID
 And CategoryName = 'seafood'
 
-Select c.CategoryName, p.ProductName, p.UnitPrice
-From Products as p join Categories as c
-Where p.CategoryID = c.CategoryID
-And CategoryName = 'seafood'
+SELECT c.CategoryName, p.ProductName, p.UnitPrice
+FROM Products AS p 
+JOIN Categories AS c ON p.CategoryID = c.CategoryID
+WHERE c.CategoryName = 'seafood'
 
 -- จงแสดงข้อมูลหมายเลขใบสั่งซื้อและชื่อบริษัทขนส่งสินค้า
 SELECT CompanyName, OrderID
@@ -33,3 +33,26 @@ SELECT CompanyName, OrderID
 FROM Orders JOIN Shippers
 ON Shippers.ShipperID=Orders.Shipvia
 WHERE OrderID=10275
+
+--ต้องการรหัสสินค้าชอสินค้า บริษัทผู้จำหน่าย ประเทศ
+SELECT p.ProductID, p.ProductName, s.CompanyName, s.Country
+FROM products p join Suppliers s on p.SupplierID = s.SupplierID
+WHERE Country in ('usa' , 'uk')
+--ต้องการรหัสพนักงาน ชื่อพนักงาน รหัสใบสั่งซื้อที่เกี่ยวข้อง เรียงตามลำดับรหัสพนักงาน
+SELECT e.EmployeeID, FirstName, o.OrderID
+from Employees e JOIN orders o ON e.EmployeeID = o.EmployeeID
+ORDER BY EmployeeID
+--ต้องการรหัสสินค้า ชื่อสินค้า เมือง และประเทศของบริษัทผูั้จำหน่าย
+
+SELECT O.OrderID เลขใบสั่งซื้อ, C.CompanyName ลูกค้า,
+E.FirstName พนักงาน, O.ShipAddress ส่งไปที่
+FROM Orders O, Customers C, Employees E
+WHERE O.CustomerID=C.CustomerID
+AND O.EmployeeID=E.EmployeeID
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
+SELECT O.OrderID เลขใบสั่งซื้อ, C.CompanyName ลูกค้า,
+E.FirstName พนักงาน, O.ShipAddress ส่งไปที่
+FROM Orders O
+join Customers C on O.CustomerID=C.CustomerID
+join Employees E on O.EmployeeID=E.EmployeeID
